3d33c95c4652471312eb961b7eef5fa5aa57e466
noahlago <ndl3389@rit.edu>
2024-02-15 16:22:22 -0500

    Beginning of Chest implementation

---
+++ src/main/java/model/Chest.java
@@ -0,0 +1,37 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Chest implements Container<Item>{
+
+    @Override
+    public boolean add(Item newItem) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public boolean remove(Item item) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+    @Override
+    public int getSize() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSize'");
+    }
+
+    @Override
+    public ArrayList<Item> items() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'items'");
+    }
+
+    @Override
+    public boolean isFull() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isFull'");
+    }
+    
+}
