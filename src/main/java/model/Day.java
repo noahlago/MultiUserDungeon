a4e93d4fef36224f3ff8565b81a896c385fb66c1
Zoe Rizzo <zjr1377@rit.edu>
2024-02-19 17:07:11 -0500

    stubbed out methods

---
+++ src/main/java/model/Day.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Day implements Cycle{
+
+    @Override
+    public void switchState() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'switchState'");
+    }
+
+    @Override
+    public void modifyDiurnalEnemies() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'modifyDiurnalEnemies'");
+    }
+
+    @Override
+    public void modifyNocturnalEnemies() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'modifyNocturnalEnemies'");
+    }
+    
+}
