046fd2b9ec06b49fd7cc93daad601339c3393e6f
MarMariMarisa <96095835+MarMariMarisa@users.noreply.github.com>
2024-02-20 10:00:13 -0500

    poushing

--- src/main/java/model/Pc.java
+++ src/main/java/model/Pc.java
@@ -2,16 +2,26 @@ package model;
 
 public class Pc extends Character {
     private Inventory inventory;
+    protected Item weaponSlot;
+    protected Item armorSlot;
     public Pc(double health, double attack, String name,Inventory inventory,int goldAmount){
         super(health,attack,name,goldAmount);
         this.inventory = inventory;
     }
     @Override
     public void takeDamage(double amount){
-        //lessen damage based off of armor
+        if(armorSlot == null || armorSlot.getDefensePercent() == 0){
+            health -= amount;
+        }else{
+            health = health - (amount * (1- armorSlot.getDefensePercent()));
+        }
     }
     @Override
     public double getAttack(){
-        
+        if(weaponSlot == null || weaponSlot.getAttack() == 0){
+            return attack;
+        }else{
+            return (attack + weaponSlot.getAttack());
+        }
     }
 }
