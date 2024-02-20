b7ff12de6b5ef89b33a63c1d5b3ee7448997b471
Zoe Rizzo <zjr1377@rit.edu>
2024-02-19 19:41:13 -0500

    room and map classes implemented

--- src/main/java/model/Map.java
+++ src/main/java/model/Map.java
@@ -1,5 +1,86 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Tiles.CharacterTile;
+import model.Tiles.ChestTile;
+import model.Tiles.EmptyTile;
+import model.Tiles.ExitTile;
+import model.Tiles.ObstacleTile;
+import model.Tiles.Tile;
+import model.Tiles.TrapTile;
+
 public class Map {
+
+    private List<Room> rooms; // list of rooms in the map
+
+    /**
+     * Map of rooms the player will go through
+     * Currently hardcoded
+     */
+    public Map(){
+        this.rooms = createRooms();
+    }
     
+    /**
+     * Creates hardcoded rooms for map
+     * @return List of rooms
+     */
+    public List<Room> createRooms(){
+        List<Room> createdRooms = new ArrayList<>();
+
+        Tile[][] tiles1 = new Tile[10][10];
+        for(int row = 0; row <= 10; row++){
+            for(int col = 0; col <= 10; col++){
+                tiles1[row][col] = new EmptyTile();
+            }
+        }
+
+        tiles1[3][5] = new ChestTile();
+        tiles1[7][2] = new CharacterTile();
+        tiles1[10][10] = new ExitTile();
+        Tile exit1 = tiles1[10][10];
+
+        Room room1 = new Room(10, 10, "Room one yippee", tiles1, true, false, exit1);
+
+        Tile[][] tiles2 = new Tile[8][8];
+        for(int row = 0; row <= 10; row++){
+            for(int col = 0; col <= 10; col++){
+                tiles2[row][col] = new EmptyTile();
+            }
+        }
+
+        tiles2[3][4] = new TrapTile();
+        tiles2[8][2] = new ObstacleTile();
+        tiles2[1][5] = new ChestTile();
+        tiles2[8][8] = new ExitTile();
+        Tile exit2 = tiles2[8][8];
+
+        Room room2 = new Room(8, 8, "Room two yippee", tiles2, false, true, exit2);
+
+        createdRooms.add(room1);
+        createdRooms.add(room2);
+
+        return createdRooms;
+    }
+
+    /**
+     * @return list of rooms in the map
+     */
+    public List<Room> getRooms(){
+        return rooms;
+    }
+
+    /**
+     * returns toString of all rooms
+     */
+    public String toString(){
+        String retVal = "";
+        for(Room room : rooms){
+            retVal += room.toString() + "\n";
+        }
+
+        return retVal;
+    }
 }
--- src/main/java/model/Room.java
+++ src/main/java/model/Room.java
@@ -3,38 +3,84 @@ package model;
 import model.Tiles.Tile;
 
 public class Room {
-    private int width;
-    private int height;
-    private String description;
-    private Tile[][] tiles;
-    private boolean isGoal;
+    private int width; // width of room
+    private int height; // height of room
+    private String description; // description of room
+    private Tile[][] tiles; // tiles within the room
+    private boolean isStart; // if room is start of map
+    private boolean isGoal; // if room is end of map
+    private Tile exit; // exit tile
 
-    public Room(int width, int height, String description, Tile[][] tiles, boolean isGoal){
+    /**
+     * Room -- defines an instance of a room
+     * 
+     * @param width
+     * @param height
+     * @param description
+     * @param tiles
+     * @param isStart
+     * @param isGoal
+     * @param exit
+     */
+    public Room(int width, int height, String description, Tile[][] tiles, boolean isStart, boolean isGoal, Tile exit){
         this.width = width;
         this.height = height;
         this.description = description;
         this.tiles = tiles;
+        this.isStart = isStart;
         this.isGoal = isGoal;
+        this.exit = exit;
     }
 
+    /**
+     * @return width of room
+     */
     public int getWidth(){
         return width;
     }
 
+    /**
+     * @return height of room
+     */
     public int getHeight(){
         return height;
     }
 
+    /**
+     * @return list of tiles in room
+     */
     public Tile[][] getTiles(){
         return tiles;
     }
 
+    /**
+     * @param x -- x coordinate
+     * @param y -- y coordinate
+     * @return tile within given coordinate
+     */
     public Tile getTile(int x, int y){
         return tiles[x][y];
     }
 
+    /**
+     * @return true if room is start, false otherwise
+     */
+    public boolean isStart(){
+        return isStart;
+    }
+
+    /**
+     * @return true if room is goal, false otherwise
+     */
     public boolean isGoal(){
         return isGoal;
+    }
+
+    /**
+     * @return exit tile
+     */
+    public Tile getExit(){
+        return exit;
     }
 
     public String toString(){
