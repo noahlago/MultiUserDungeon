e213cb41e88533c1c1fd0a474b1c2e0b7d88160e
Zoe Rizzo <zjr1377@rit.edu>
2024-02-22 10:23:05 -0500

    added game over and getcycle

--- src/main/java/model/MUD.java
+++ src/main/java/model/MUD.java
@@ -55,6 +55,7 @@ public class MUD {
     public Character getPlayer(){
         return this.player;
     }
+
     /**
      * @return name of user
      */
@@ -96,7 +97,7 @@ public class MUD {
      * Increases number of turns
      */
     public void uptickTurns(){
-        numTurns+=1;
+        numTurns += 1;
     }
 
     /**
@@ -116,6 +117,13 @@ public class MUD {
     }
 
     /**
+     * @return current state of cycle
+     */
+    public Cycle getCycle(){
+        return cycle;
+    }
+
+    /**
      * Checks the turn count and switches state of the cycle if interval of 10
      * Prints out current state
      */
@@ -132,11 +140,26 @@ public class MUD {
         }
     }
 
+    /**
+     * Checks if the game is over
+     * Tells user if they won or lost if game over
+     * @return true if character health is <= 0 or character on exit tile of goal room, false otherwise
+     */
+    public boolean gameOver(){
+        if(getHealth() <= 0){
+            System.out.println("You lost!");
+            return true;
+        }
+        // else if (/*character on exit tile in goal room*/){
+        //     System.out.println("You won!");
+        // }
+        return false;
+    }
+
     @Override
     public String toString(){
         return this.currentRoom.toString();
     }
-
 
     /*
      * to do:
@@ -144,15 +167,11 @@ public class MUD {
      * add visitor functionality (move to another tile)
      * attack npcs
      * npcs attack pcs after each round
-     * day / night cycle
      * move to next room (if tile is exit)
      * use items 
      * equip weapons or armor
      * win game if tile is exit and room is goal
-     * lose game if pc health <= 0
      */
-
-
 
     public static void main(String[] args) {
         Pc play = new Pc(1,10,"mars",new Inventory(),100);
