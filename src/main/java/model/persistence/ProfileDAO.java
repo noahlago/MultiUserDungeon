e08a9e5dee6f5d641876fc5ea0461b6861924942
noahlago <ndl3389@rit.edu>
2024-03-19 10:42:24 -0400

    Profile CSV DAO implemented, just missing save method

--- src/main/java/model/persistence/ProfileDAO.java
+++ src/main/java/model/persistence/ProfileDAO.java
@@ -1,13 +1,19 @@
 package model.persistence;
 
+import java.io.IOException;
 import java.util.Collection;
 
 import model.User;
 
+/**
+ * This class defines the behavior for a FileDAO to manage all user profiles that have been created for the MUD game. Instances of this class will allow user profiles to be 
+ * accessed, created, deleted, and logged in to. 
+ * @author Noah Lago ndl3389@rit.edu
+ */
 public interface ProfileDAO {
     public Collection<User> getAllUsers();
-    public User getUser(String username);
+    public User getUser(String username) throws IOException;
     public boolean newUser(User newUser);
     public boolean deleteUser(String username);
-    public User logIn(String username, String password);
+    public User logIn(String username, String password) throws IOException;
 }
