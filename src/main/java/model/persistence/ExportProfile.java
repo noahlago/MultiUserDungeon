80023693768c6b1af4d840b52edf004afdcde254
noahlago <ndl3389@rit.edu>
2024-03-23 13:50:58 -0400

    can now export a profile to a csv file

---
+++ src/main/java/model/persistence/ExportProfile.java
@@ -0,0 +1,46 @@
+package model.persistence;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import model.User;
+
+public class ExportProfile {
+    private ProfileCSVFileDAO profileManager;
+
+    public ExportProfile(ProfileCSVFileDAO profileManager){
+        this.profileManager = profileManager;
+    }
+
+    public void exportCSV(String filename, String username) throws IOException{
+        File exportFile = makeFile(filename);
+        FileWriter writer = new FileWriter(exportFile);
+        User user = profileManager.getUser(username);
+
+        writer.append(username + "," + user.getPassword() + "," + user.getGamesPlayed() + "," + user.getLivesLost() + "," + user.getMonstersKilled() + "," + user.getTotalGold() + "," + user.getItemsFound() + "\n");
+
+        writer.flush();
+        writer.close();
+    }
+    
+    public void exportXML(String filename, String username){
+        //TODO
+    }
+
+    public void exportJSON(String filename, String username){
+        //TODO
+    }
+
+    private File makeFile(String filename) throws IOException{
+        File exportFile = new File(filename);
+        if(exportFile.createNewFile()){
+            System.out.println("Fle: '" + filename + "' successfully created.");
+        }else{
+            System.out.println("File: '" + filename + "' already exists. ");
+            throw new IOException("Export file coult not be created. Filename already in use. ");
+        }
+
+        return exportFile;
+    }
+}
---
+++ src/main/java/model/persistence/ImportProfile.java
@@ -0,0 +1,15 @@
+package model.persistence;
+
+public class ImportProfile {
+    public void importCSV(String filename){
+        //TODO
+    }
+
+    public void importXML(String filename){
+        //TODO
+    }
+
+    public void importJSON(String filename){
+        //TODO
+    }
+}
