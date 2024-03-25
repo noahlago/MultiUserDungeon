e24993d5e8c7ae0c1967b024096283476793c7e5
Michael DiBiase <MJD0904@Gmail.com>
2024-03-25 19:09:51 -0400

    Added gold value to items

--- src/main/java/model/LameRags.java
+++ src/main/java/model/LameRags.java
@@ -2,7 +2,7 @@ package model;
 
 public class LameRags extends Item{
     public LameRags(String name, String description){
-        super(name, description, ItemType.ARMOR);
+        super(name, description, ItemType.ARMOR, 20);
         super.defensePercent += 0.1;
     }
 }
