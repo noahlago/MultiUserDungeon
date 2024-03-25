bd1d78ade04ade900a1ab936559ca77c8b1c184d
noahlago <ndl3389@rit.edu>
2024-03-25 19:52:35 -0400

    beginning to add exporting functionality for xmls

--- src/main/java/model/persistence/ExportProfile.java
+++ src/main/java/model/persistence/ExportProfile.java
@@ -4,8 +4,12 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -31,7 +35,15 @@ public class ExportProfile {
     }
     
     public void exportXML(String filename, String username) throws IOException, ParserConfigurationException, SAXException{
-        File exportFile = makeFile(filename);
+        User user = profileManager.getUser(username);
+
+        DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document doc = builder.newDocument();
+        Element userElement = doc.createElement("profile");
+        
+        Element usernameElement = doc.createElement("username");
+        usernameElement.appendChild(doc.createTextNode(user.getUsername()));
+        userElement.appendChild(usernameElement);
     }
 
     public void exportJSON(String filename, String username) throws IOException{
