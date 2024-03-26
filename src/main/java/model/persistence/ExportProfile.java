ddeb82af5353969a49a617838ff7de85613407fe
noahlago <ndl3389@rit.edu>
2024-03-26 09:55:42 -0400

    please let this be the end of xml shenanigans

--- src/main/java/model/persistence/ExportProfile.java
+++ src/main/java/model/persistence/ExportProfile.java
@@ -1,12 +1,20 @@
 package model.persistence;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -34,7 +42,8 @@ public class ExportProfile {
         writer.close();
     }
     
-    public void exportXML(String filename, String username) throws IOException, ParserConfigurationException, SAXException{
+    public void exportXML(String filename, String username) throws IOException, ParserConfigurationException, SAXException, TransformerFactoryConfigurationError, TransformerException{
+        File exportFile = makeFile(filename);
         User user = profileManager.getUser(username);
 
         DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
@@ -44,6 +53,40 @@ public class ExportProfile {
         Element usernameElement = doc.createElement("username");
         usernameElement.appendChild(doc.createTextNode(user.getUsername()));
         userElement.appendChild(usernameElement);
+
+        Element passwordElement = doc.createElement("password");
+        passwordElement.appendChild(doc.createTextNode(user.getPassword()));
+        userElement.appendChild(passwordElement);
+
+        Element gamesPlayed = doc.createElement("gamesPlayed");
+        gamesPlayed.appendChild(doc.createTextNode("" + user.getGamesPlayed()));
+        userElement.appendChild(gamesPlayed);
+
+        Element livesLost = doc.createElement("livesLost");
+        livesLost.appendChild(doc.createTextNode("" + user.getGamesPlayed()));
+        userElement.appendChild(livesLost);
+
+        Element monstersKilled = doc.createElement("monstersKilled");
+        monstersKilled.appendChild(doc.createTextNode("" + user.getGamesPlayed()));
+        userElement.appendChild(monstersKilled);
+
+        Element totalGold = doc.createElement("totalGold");
+        totalGold.appendChild(doc.createTextNode("" + user.getGamesPlayed()));
+        userElement.appendChild(totalGold);
+
+        Element itemsFound = doc.createElement("itemsFound");
+        itemsFound.appendChild(doc.createTextNode("" + user.getGamesPlayed()));
+        userElement.appendChild(itemsFound);
+
+        doc.appendChild(userElement);
+
+        Transformer transformer = TransformerFactory.newInstance().newTransformer();
+
+        transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+        transformer.transform(new DOMSource(doc), new StreamResult(new FileOutputStream(exportFile)));
     }
 
     public void exportJSON(String filename, String username) throws IOException{
