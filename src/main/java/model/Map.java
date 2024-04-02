37d341b7598e3c43759e8989fbc3f38a81bb1809
Eliot Seol <eys7709@g.rit.edu>
2024-04-01 22:13:34 -0400

    Cleaned up room generation by making a helper function, and let merchants spawn as well

--- src/main/java/model/Map.java
+++ src/main/java/model/Map.java
@@ -27,14 +27,13 @@ public class Map {
     @JsonProperty("rooms") private List<Room> rooms; // list of rooms in the map
     @JsonProperty("player") private Character player;
 
-    @JsonCreator 
-    public Map(@JsonProperty("rooms") List<Room> rooms){
-        this.rooms = rooms;
-    }
-
+    /**
+     * Map of rooms the player will go through
+     * Currently hardcoded
+     */
     @JsonCreator 
     public Map(){
-        this.rooms = null;
+        this.rooms = createRooms();
     }
 
     public void setPlayer(Character player) {
@@ -100,9 +99,75 @@ public class Map {
         Collections.shuffle(master_list);
         return master_list;
     }
+
+    public void randomlyGenerate(int x_dimension, int y_dimension, int item_num, String generation, List<String> occupied_spots, ConcreteTile[][] room) {
+        Random rand = new Random();
+        for (int i = 0; i <= item_num; i++) {
+            int random_x = rand.nextInt(x_dimension);
+            int random_y = rand.nextInt(y_dimension);
+
+            String x_val = String.valueOf(random_x);
+            String y_val = String.valueOf(random_y);
+            String result = x_val + y_val;
+
+            if (occupied_spots.contains(result)) {
+                item_num++;
+                continue;
+            }
+
+            if (generation == "NPC") {
+                occupied_spots.add(result);
+                List<Npc> enemy_list = createNPCList();
+                Npc character = enemy_list.get(0);
+                room[random_x][random_y] = new CharacterTile(character, random_x, random_y);
+            }
+
+            if (generation == "Trap") { 
+                occupied_spots.add(result);
+                room[random_x][random_y] = new TrapTile("Spike Trap", "Deadly Spikes, ouch!");
+            }
+
+            if (generation == "Obstacle") {
+                occupied_spots.add(result);
+                room[random_x][random_y] = new ObstacleTile("Big #@!%$ Boulder");
+            }
+
+            if (generation == "Chest") {
+                List<Item> loot_items = createItemList();
+                int item_amount = rand.nextInt(5) + 1;
+                Chest chest = new Chest(null);
+    
+                for (int x = 0; x < item_amount; x++) {
+                    Item item = loot_items.get(x);
+                    chest.add(item);
+                }
+    
+                occupied_spots.add(result);
+                room[random_x][random_y] = new ChestTile(chest);
+            }
+
+            if (generation == "Merchant") {
+                List<Boolean> chance = new ArrayList<>();
+                chance.add(true)
+                for (int k = 0; k <=4; k++) {
+                    chance.add(false);
+                }
+                Collections.shuffle(chance);
+                Boolean condition = chance.get(0);
+
+                if (condition) {
+                    occupied_spots.add(result);
+                    room[random_x][random_y] = new MerchantTile();
+                }
+            }
+
+
+        }
+
+    }
         
        
-    public ConcreteTile populateRoom(int x_dimension, int y_dimension, ConcreteTile[][] room, int exitX, int exitY) {
+    public ConcreteTile populateRoom(int x_dimension, int y_dimension, ConcreteTile[][] room) {
         Random rand = new Random();
         int trap_num = rand.nextInt(6);
         int obstacle_num = rand.nextInt(6);
@@ -111,137 +176,208 @@ public class Map {
         List<String> occupied_spots = new ArrayList<>();
         occupied_spots.add("00");
 
-        for (int i = 0; i <= opp_num; i++) {
-            int random_x = rand.nextInt(x_dimension);
-            int random_y = rand.nextInt(y_dimension);
-
-            String x_val = String.valueOf(random_x);
-            String y_val = String.valueOf(random_y);
-            String result = x_val + y_val;
-
-            if (occupied_spots.contains(result)) {
-                opp_num++;
-                continue;
-            }
-
-            occupied_spots.add(result);
-            List<Npc> enemy_list = createNPCList();
-            Npc character = enemy_list.get(0);
-            room[random_x][random_y] = new CharacterTile(character, random_x, random_y);
-        }
-
-        for (int i = 0; i <= trap_num; i++) {
-            int random_x = rand.nextInt(x_dimension);
-            int random_y = rand.nextInt(y_dimension);
-            if (random_x == x_dimension-1 && random_y == y_dimension-1) {
-                trap_num++;
-                continue;
-            }
-
-            if (random_x == 0 && random_y == 1 || random_x == 1 && random_y == 0) {
-                trap_num++;
-                continue;
-            }
-
-            String x_val = String.valueOf(random_x);
-            String y_val = String.valueOf(random_y);
-            String result = x_val + y_val;
-
-            if (occupied_spots.contains(result)) {
-                trap_num++;
-                continue;
-            }
-            
-            occupied_spots.add(result);
-            room[random_x][random_y] = new TrapTile("Spike Trap", "Deadly Spikes, ouch!");
-        }
-
-        for (int i = 0; i <= obstacle_num; i++) {
-            int random_x = rand.nextInt(x_dimension-1);
-            int random_y = rand.nextInt(y_dimension-1);
-            if (random_x == x_dimension-1 && random_y == y_dimension-1) {
-                obstacle_num++;
-                continue;
-            }
-
-            if (random_x == 0 && random_y == 1) {
-                obstacle_num++;
-                continue;
-            }
-
-            if (random_x == 1 && random_y == 0) {
-                obstacle_num++;
-                continue;
-            }
-
-
-            String x_val = String.valueOf(random_x);
-            String y_val = String.valueOf(random_y);
-            String result = x_val + y_val;
-
-            if (occupied_spots.contains(result)) {
-                obstacle_num++;
-                continue;
-            }
-            
-            occupied_spots.add(result);
-            room[random_x][random_y] = new ObstacleTile("Big #@!%$ Boulder");
-        }
-
-        for (int i = 0; i <= chest_num; i++) {
-            int random_x = rand.nextInt(x_dimension);
-            int random_y = rand.nextInt(y_dimension);
-            if (random_x == x_dimension-1 && random_y == y_dimension-1) {
-                chest_num++;
-                continue;
-            }
-
-            if (random_x == 0 && random_y == 1) {
-                chest_num++;
-                continue;
-            }
-
-            if (random_x == 1 && random_y == 0) {
-                chest_num++;
-                continue;
-            }
-
-
-            String x_val = String.valueOf(random_x);
-            String y_val = String.valueOf(random_y);
-            String result = x_val + y_val;
-
-            if (occupied_spots.contains(result)) {
-                chest_num++;
-                continue;
-            }
-            
-            List<Item> loot_items = createItemList();
-            int item_amount = rand.nextInt(5) + 1;
-            Chest chest = new Chest(null);
-
-            for (int x = 0; x < item_amount; x++) {
-                Item item = loot_items.get(x);
-                chest.add(item);
-            }
-
-            occupied_spots.add(result);
-            room[random_x][random_y] = new ChestTile(chest);
-        }
-
-        room[exitX][exitY] = new ExitTile();
-        ConcreteTile exit1 = room[exitX][exitY];
+        // Adds npcs to a room using a helper function
+        randomlyGenerate(x_dimension, y_dimension, opp_num, "NPC", occupied_spots, room);
+        // Adds Traps to a room using a helper function
+        randomlyGenerate(x_dimension, y_dimension, trap_num, "Trap", occupied_spots, room);
+        // Adds Obstacles to a room using a helper function
+        randomlyGenerate(x_dimension, y_dimension, obstacle_num, "Obstacle", occupied_spots, room);
+        // Adds Chest to a room using a helper function
+        randomlyGenerate(x_dimension, y_dimension, chest_num, "Chest", occupied_spots, room);
+        // Adds a randomly generated merchant
+        randomlyGenerate(x_dimension, y_dimension, 1, "Merchant", occupied_spots, room);
+        ConcreteTile exit1 = room[9][9];
         room[0][0] = new CharacterTile(player, 0, 0);
 
-        //TODO delete this line, for testing purposes only
-        room[3][3] = new MerchantTile();
+        while (true) {
+            int coin_flip = rand.nextInt(2);
+            if (coin_flip == 1) {
+                int random_x = rand.nextInt(x_dimension);
+                int coin_flip_2 = rand.nextInt(2);
+                String x_val = String.valueOf(random_x);
+                String y_val = String.valueOf(coin_flip_2);
+                String result = x_val + y_val;
+                if (random_x == 0 && coin_flip_2 == 0) {
+                    continue;
+                }
 
-        
+                if (random_x == 0 && coin_flip_2 == 1) {
+                    continue;
+                }
+
+                if (random_x == 0 && coin_flip_2 == 2) {
+                    continue;
+                }
+
+                if (random_x == 1 && coin_flip_2 == 0) {
+                    continue;
+                }
+
+                if (random_x == 2 && coin_flip_2 == 0) {
+                    continue;
+                }
+    
+                if (occupied_spots.contains(result)) {
+                    continue;
+                }
+
+                
+
+                if (coin_flip_2 == 1) {
+                    if (random_x == 0) {
+                        if (occupied_spots.contains("01") && occupied_spots.contains("10")) {
+                            continue;
+                        }
+                    }
+                    if (random_x == 9) {
+                        if (occupied_spots.contains("89") && occupied_spots.contains("98")) {
+                            continue;
+                        }
+                    }
+
+                    String add = String.valueOf(random_x+1);
+                    String block1 = add + "0";
+
+                    String cord1 = String.valueOf(random_x-1);
+                    String block2 = cord1 + "0";
+
+                    String cord2 = String.valueOf(random_x);
+                    String block3 = cord2 + "1";
+                    
+                    if (occupied_spots.contains(block3) && occupied_spots.contains(block2) && occupied_spots.contains(block1)) {
+                        continue;
+                    }
+
+                    add = String.valueOf(random_x+1);
+                    block1 = add + "9";
+
+                    cord1 = String.valueOf(random_x-1);
+                    block2 = cord1 + "9";
+
+                    cord2 = String.valueOf(random_x);
+                    block3 = cord2 + "8";
+                    
+                    if (occupied_spots.contains(block3) && occupied_spots.contains(block2) && occupied_spots.contains(block1)) {
+                        continue;
+                    }
+                    
+                    room[random_x][0] = new ExitTile();
+                    exit1 = room[random_x][0];
+                    break;
+                }
+                else {
+                    room[random_x][9] = new ExitTile();
+                    exit1 = room[random_x][9];
+                    break;
+                }
+            }
+            else {
+                int random_y = rand.nextInt(y_dimension);
+                int coin_flip_2 = rand.nextInt(2);
+                String x_val = String.valueOf(coin_flip_2);
+                String y_val = String.valueOf(random_y);
+                String result = x_val + y_val;
+
+                if (coin_flip_2 == 0 && random_y == 0) {
+                    continue;
+                }
+
+                if (coin_flip_2 == 0 && random_y == 1) {
+                    continue;
+                }
+
+                if (coin_flip_2 == 0 && random_y == 2) {
+                    continue;
+                }
+
+                if (coin_flip_2 == 1 && random_y == 0) {
+                    continue;
+                }
+
+                if (coin_flip_2 == 2 && random_y == 0) {
+                    continue;
+                }
+    
+                if (occupied_spots.contains(result)) {
+                    continue;
+                }
+                
+                if (coin_flip_2 == 1) {
+                    if (random_y == 0) {
+                        if (occupied_spots.contains("01") && occupied_spots.contains("10")) {
+                            continue;
+                        }
+                    }
+                    if (random_y == 9) {
+                        if (occupied_spots.contains("89") && occupied_spots.contains("98")) {
+                            continue;
+                        }
+                    }
+
+                    String add = String.valueOf(random_y+1);
+                    String block1 = "0" + add;
+
+                    String cord1 = String.valueOf(random_y-1);
+                    String block2 = "0" + cord1;
+
+                    String cord2 = String.valueOf(random_y);
+                    String block3 = "1" + cord2;
+                    
+                    if (occupied_spots.contains(block3) && occupied_spots.contains(block2) && occupied_spots.contains(block1)) {
+                        continue;
+                    }
+
+                    add = String.valueOf(random_y+1);
+                    block1 = "9" + add;
+
+                    cord1 = String.valueOf(random_y-1);
+                    block2 = "9" + cord1;
+
+                    cord2 = String.valueOf(random_y);
+                    block3 = "8" + cord2;
+                    
+                    if (occupied_spots.contains(block3) && occupied_spots.contains(block2) && occupied_spots.contains(block1)) {
+                        continue;
+                    }
+
+                    room[0][random_y] = new ExitTile();        
+                    exit1 = room[0][random_y];
+                    break;
+                }
+                else {
+                    room[9][random_y] = new ExitTile();
+                    exit1 = room[9][random_y];
+                    break;
+                }
+            }
+        }
         return exit1;
     }
+    
+    /**
+     * Creates hardcoded rooms for map
+     * @return List of rooms
+     */
+    public List<Room> createRooms(){
+        List<Room> createdRooms = new ArrayList<>();
+        // Creates room based on dimensions
+        ConcreteTile[][] tiles1 = createRoom(10,10);
+        // Randomly populates room with traps, obsticales, and chests. Returns Exit located at random position
+        ConcreteTile exit1 = populateRoom(10, 10, tiles1);
+        Npc[] npcs1 = {};
+        // fully creates the room
+        Room room1 = new Room(10, 10, "Room one: The begining of the journey", tiles1, true, false, exit1, npcs1);
 
-    public int numRooms(){
-        return rooms.size();
+
+        ConcreteTile[][] tiles2 = createRoom(10,10);
+        ConcreteTile exit2 = populateRoom(10,10,tiles2);
+        Npc[] npcs2 = {};
+        Room room2 = new Room(10, 10, "Room two yippee", tiles2, false, true, exit2, npcs2);
+
+        createdRooms.add(room1);
+        createdRooms.add(room2);
+
+        return createdRooms;
     }
 
     /**
